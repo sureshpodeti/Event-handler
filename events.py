d56a4f46e839f5878d33eeac833e72d138918b18
spodeti <spodeti@cigital.com>
2016-07-08 23:35:09 +0530

    Modified functionalities, need to include errors section

--- events.py
+++ events.py
@@ -18,11 +18,15 @@ import time
 def add_event(event_string):
  s=event_string.split('-')
  d={s[0]:s[1]}
- date=time.strftime('%x')
+ date=time.strftime('%d.%m.%Y')
  if os.path.isfile('suresh.json'):
   with open('suresh.json') as fh:
    data=json.load(fh)
-  data[date].update(d)
+   if date in data:
+    data[date].update(d)
+   else:
+    t={date:d}
+    data.update(t)
   with open('suresh.json','w') as fh:
    json.dump(data,fh)
   
@@ -34,10 +38,11 @@ def add_event(event_string):
 
 #function to delete an event by date(event id)
 def delete_event(event_id=None):
+ s=event_id.split('-')
  if os.path.isfile('suresh.json'):
   with open('suresh.json') as fh:
    data=json.load(fh)
-   del data[event_id]
+   del data[s[0]][s[1]]
   with open('suresh.json','w') as fh:
    json.dump(data,fh)
  else:
@@ -48,10 +53,11 @@ def delete_event(event_id=None):
 
 #function to search event by date 
 def search_event(event_id=None):
+ s=event_id.split('-')
  if os.path.isfile('suresh.json'):
   with open('suresh.json') as fh:
    data=json.load(fh)
-   return data[event_id]
+   return data[s[0]][s[1]]
  else:
    print "No event exist in the list"
 
@@ -70,7 +76,7 @@ def main():
  parser.add_argument('-a','--add',help='adds an event to the list',action='store',type=str)
  parser.add_argument('-d','--delete',help='delete an event by date format dd/mm/yy',action='store',type=str)
  parser.add_argument('-s','--search',help='search an event by date format dd/mm/yy',action='store',type=str)
- parser.add_argument('-l','--completelist',help='show the list of all events')
+ parser.add_argument('-l','--completelist',help='show the list of all events',action='store_true')
  args=parser.parse_args()
  
  if args.add:
@@ -80,7 +86,7 @@ def main():
  if args.search:
   print  search_event(args.search)
  if args.completelist:
-  print show_list()
+  print json.dumps(show_list())
 
 if __name__=='__main__':
  main()
